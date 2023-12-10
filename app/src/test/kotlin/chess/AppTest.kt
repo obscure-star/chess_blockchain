f42e33c11d7e77e7900df5d956559e9f2ab3996e
obscure-star <iibeanusiwork@gmail.com>
2023-12-10 12:43:32 -0800

    add tests

--- app/src/test/kotlin/chess/AppTest.kt
+++ app/src/test/kotlin/chess/AppTest.kt
@@ -101,7 +101,137 @@ class AppTest {
     }
 
     @Test
-    fun `Kasparov vs Topalov, Wijk aan Zee 1999 game play test`() {
+    fun `the italian defense (starter move)`() {
+        provideInput("white", "e2-e4", "e7-e5", "g1-f3", "b8-c6", "f1-c4", "q")
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+        val pieceFinalPosition = game.firstPlayer.selectedPiece!!.position
+        val pieceInitialPosition = game.firstPlayer.destinationPiece!!.position
+
+        assertEquals(
+            "white_bishop",
+            board[
+                8 - pieceFinalPosition.row,
+            ][pieceFinalPosition.column.toColumnNumber()].name,
+        )
+        assertEquals(
+            "empty",
+            board[8 - pieceInitialPosition.row][pieceInitialPosition.column.toColumnNumber()].name,
+        )
+    }
+
+    @Test
+    fun `the sicilian defense (starter move)`() {
+        provideInput("white", "e2-e4", "c7-c5", "q")
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+        val pieceFinalPosition = game.secondPlayer.selectedPiece!!.position
+        val pieceInitialPosition = game.secondPlayer.destinationPiece!!.position
+
+        assertEquals(
+            "black_pawn",
+            board[
+                8 - pieceFinalPosition.row,
+            ][pieceFinalPosition.column.toColumnNumber()].name,
+        )
+        assertEquals(
+            "empty",
+            board[8 - pieceInitialPosition.row][pieceInitialPosition.column.toColumnNumber()].name,
+        )
+    }
+
+    @Test
+    fun `the frensh defense (starter move)`() {
+        provideInput("white", "e2-e4", "e7-e6", "d2-d4", "d7-d5", "q")
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+        val pieceFinalPosition = game.secondPlayer.selectedPiece!!.position
+        val pieceInitialPosition = game.secondPlayer.destinationPiece!!.position
+
+        assertEquals(
+            "black_pawn",
+            board[
+                8 - pieceFinalPosition.row,
+            ][pieceFinalPosition.column.toColumnNumber()].name,
+        )
+        assertEquals(
+            "empty",
+            board[8 - pieceInitialPosition.row][pieceInitialPosition.column.toColumnNumber()].name,
+        )
+    }
+
+    @Test
+    fun `the Ruy-Lopez (starter move)`() {
+        provideInput("white", "e2-e4", "e7-e5", "g1-f3", "b8-c6", "f1-b5", "q")
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+        val pieceFinalPosition = game.firstPlayer.selectedPiece!!.position
+        val pieceInitialPosition = game.firstPlayer.destinationPiece!!.position
+
+        assertEquals(
+            "white_bishop",
+            board[
+                8 - pieceFinalPosition.row,
+            ][pieceFinalPosition.column.toColumnNumber()].name,
+        )
+        assertEquals(
+            "empty",
+            board[8 - pieceInitialPosition.row][pieceInitialPosition.column.toColumnNumber()].name,
+        )
+    }
+
+    @Test
+    fun `the slav defense (starter move)`() {
+        provideInput("white", "d2-d4", "d7-d5", "c2-c4", "c7-c6", "q")
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+        val pieceFinalPosition = game.secondPlayer.selectedPiece!!.position
+        val pieceInitialPosition = game.secondPlayer.destinationPiece!!.position
+
+        assertEquals(
+            "black_pawn",
+            board[
+                8 - pieceFinalPosition.row,
+            ][pieceFinalPosition.column.toColumnNumber()].name,
+        )
+        assertEquals(
+            "empty",
+            board[8 - pieceInitialPosition.row][pieceInitialPosition.column.toColumnNumber()].name,
+        )
+    }
+
+    @Test
+    fun `Kasparov (white) vs Topalov (black), Wijk aan Zee 1999 game play test`() {
         provideInput(
             "white",
             "e2-e4",
@@ -217,7 +347,7 @@ class AppTest {
     }
 
     @Test
-    fun `Morphy vs Allies, Paris Opera 1858 game play test`() {
+    fun `Morphy (white) vs Allies (black), Paris Opera 1858 game play test (white wins)`() {
         provideInput(
             "white",
             "e2-e4",
@@ -274,6 +404,178 @@ class AppTest {
         )
 
         assertTrue(game.firstPlayer.winner)
+    }
+
+    @Test
+    fun `Aronian (white) vs Anand (black), Wijk aan Zee 2013`() {
+        provideInput(
+            "white",
+            "d2-d4",
+            "d7-d5",
+            "c2-c4",
+            "c7-c6",
+            "g1-f3",
+            "g8-f6",
+            "b1-c3",
+            "e7-e6",
+            "e2-e3",
+            "b8-d7",
+            "f1-d3",
+            "d5-c4",
+            "d3-c4",
+            "b7-b5",
+            "c4-d3",
+            "f8-d6",
+            "e1-g1",
+            "e8-g8",
+            "d1-c2",
+            "c8-b7",
+            "a2-a3",
+            "a8-c8",
+            "f3-g5",
+            "c6-c5",
+            "g5-h7",
+            "f6-g4",
+            "f2-f4",
+            "c5-d4",
+            "e3-d4",
+            "d6-c5",
+            "d3-e2",
+            "d7-e5",
+            "e2-g4",
+            "c5-d4",
+            "g1-h1",
+            "e5-g4",
+            "h7-f8",
+            "f7-f5",
+            "f8-g6",
+            "d8-f6",
+            "h2-h3",
+            "f6-g6",
+            "c2-e2",
+            "g6-h5",
+            "e2-d3",
+            "d4-e3",
+            "q",
+        )
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+
+        assertEquals(
+            "white_king",
+            board[7][7].name,
+        )
+    }
+
+    @Test
+    fun `Byrne (white) vs Fisher (black), New York 1956 (black wins)`() {
+        provideInput(
+            "white",
+            "g1-f3",
+            "g8-f6",
+            "c2-c4",
+            "g7-g6",
+            "b1-c3",
+            "f8-g7",
+            "d2-d4",
+            "e8-g8",
+            "c1-f4",
+            "d7-d5",
+            "d1-b3",
+            "d5-c4",
+            "b3-c4",
+            "c7-c6",
+            "e2-e4",
+            "b8-d7",
+            "a1-d1",
+            "d7-b6",
+            "c4-c5",
+            "c8-g4",
+            "f4-g5",
+            "b6-a4",
+            "c5-a3",
+            "a4-c3",
+            "b2-c3",
+            "f6-e4",
+            "g5-e7",
+            "d8-b6",
+            "f1-c4",
+            "e4-c3",
+            "e7-c5",
+            "f8-e8",
+            "e1-f1",
+            "g4-e6",
+            "c5-b6",
+            "e6-c4",
+            "f1-g1",
+            "c3-e2",
+            "g1-f1",
+            "e2-d4",
+            "f1-g1",
+            "d4-e2",
+            "g1-f1",
+            "e2-c3",
+            "f1-g1",
+            "a7-b6",
+            "a3-b4",
+            "a8-a4",
+            "b4-b6",
+            "c3-d1",
+            "h2-h3",
+            "a4-a2",
+            "g1-h2",
+            "d1-f2",
+            "h1-e1",
+            "e8-e1",
+            "b6-d8",
+            "g7-f8",
+            "f3-e1",
+            "c4-d5",
+            "e1-f3",
+            "f2-e4",
+            "d8-b8",
+            "b7-b5",
+            "h3-h4",
+            "h7-h5",
+            "f3-e5",
+            "g8-g7",
+            "h2-g1",
+            "f8-c5",
+            "g1-f1",
+            "e4-g3",
+            "f1-e1",
+            "c5-b4",
+            "e1-d1",
+            "d5-b3",
+            "d1-c1",
+            "g3-e2",
+            "c1-b1",
+            "e2-c3",
+            "b1-c1",
+            "a2-c2",
+            "q",
+        )
+
+        main()
+
+        val game = Game.getCurrentGame()
+        assertTrue(game?.firstPlayer is WhitePlayer)
+        assertTrue(game?.secondPlayer is BlackPlayer)
+
+        val board = game!!.board.board
+
+        assertEquals(
+            "white_king",
+            board[7][2].name,
+        )
+
+        assertTrue(game.secondPlayer.winner)
     }
 
     private fun provideInput(vararg inputs: String) {
